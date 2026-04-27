cadc898f3be3cf1d8fb86272b00d100136458aad
Ngoc Nguyen <nguyengoc121203@gmail.com>
2026-04-27 17:49:30 +0000

    finished module

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -9,13 +9,26 @@ function Header({ name, year }) {
   );
 }
 
+const items = [
+  "Mac and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables",
+];
+
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li style={{ listStyleType: "none" }}>{dish}</li>
+      ))}
+    </ul>
+  );
+}
 function App() {
   return (
     <div>
       <Header name="Alex" year={new Date().getFullYear()} />
-      <main>
-        <h2>We serve the most delicious food around</h2>
-      </main>
+      <Main dishes={items} />
     </div>
   );
 }
